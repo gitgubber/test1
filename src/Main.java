08806d591416bb26bebf24b3671491f6b71f0ff0
altuk <al.tukanov@gmail.com>
2021-09-30 10:43:25 +0400

    try4

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //Вывести на консоль фразу Hello world 10 Раз
-        int counter = 0;
-        while(counter < 3) {
-            System.out.println(counter+" hello world");
-            counter++;
-            //counter = counter + 1;
-        }
-        System.out.println("test");
+        System.out.println("one");
         System.out.println("develop continue11");
     }
 }
