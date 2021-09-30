8a7fe316acbdd0552614618c6aedc4d3dbf7d677
gitgubber <85120414+gitgubber@users.noreply.github.com>
2021-09-30 17:28:01 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("one");
-        System.out.println("develop continue11");
-        System.out.println("develop continue12");
-        System.out.println("develop continue13");
-        System.out.println("develop continue14");
-        System.out.println("develop continue15");
-        System.out.println("develop continue16");
-        System.out.println("develop continue17");
-        System.out.println("develop continue118");
+      
     }
 }
