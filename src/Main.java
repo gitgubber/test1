01f67a1fbb025de6018105801085d13e8ea33c00
altuk <al.tukanov@gmail.com>
2021-09-30 10:45:07 +0400

    m try4

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("one");
         System.out.println("develop continue11");
+        System.out.println("develop continue12");
+        System.out.println("develop continue13");
+        System.out.println("develop continue14");
+        System.out.println("develop continue15");
     }
 }
