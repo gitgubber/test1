d3ebf74f0a338230bb52778f7b6063d626f44c22
altuk <al.tukanov@gmail.com>
2021-09-30 09:55:50 +0400

    init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Вывести на консоль фразу Hello world 10 Раз
+        int counter = 0;
+        while(counter < 3) {
+            System.out.println(counter+" hello world");
+            counter++;
+            //counter = counter + 1;
+        }
+        System.out.println("fininsh");
+
+    }
+}
